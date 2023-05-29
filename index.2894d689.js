02207e58241f440a95f8e13e38daf2331afe7960
chertoha <a.chertok@akatech.com.ua>
2023-05-29 11:09:14 +0000

    Deploying to gh-pages from @ chertoha/iconik-event-atelier@c4386f135e8b85ab1ff8db30185dde54b31ed674 🚀

---
+++ index.2894d689.js
@@ -0,0 +1,2 @@
+!function(){let e=document.querySelector(".js-services-btn"),s=document.querySelector(".js-services-list");e.addEventListener("click",()=>{s.classList.add("is-open")}),s.addEventListener("click",t=>{t.target.classList.contains("js-services-label")&&(e.innerText=t.target.innerText,s.classList.remove("is-open"))})}();
+//# sourceMappingURL=index.2894d689.js.map
---
+++ index.3cdfcc66.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".js-services-btn"),s=document.querySelector(".js-services-list");e.addEventListener("click",()=>{s.classList.add("is-open")}),s.addEventListener("click",t=>{t.target.classList.contains("js-services-label")&&(e.innerText=t.target.innerText,s.classList.remove("is-open"))});
+//# sourceMappingURL=index.3cdfcc66.js.map
