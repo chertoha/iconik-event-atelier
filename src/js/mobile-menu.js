2dc382090dfc6e3dece1b64e6c35e0019335da03
Anton <a.chertok@akatech.com.ua>
2023-06-28 00:30:35 +0300

    menu mob

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,6 +1,7 @@
 const mobileMenu = document.querySelector(".js-menu");
 const openBtn = document.querySelector(".js-open-menu");
 const closeBtn = document.querySelector(".js-close-menu");
+const headerNavList = document.querySelector("#js-header-nav-list");
 
 openBtn.addEventListener("click", () => {
   mobileMenu.classList.add("is-open");
@@ -8,6 +9,7 @@ openBtn.addEventListener("click", () => {
   document.body.classList.add("no-scroll");
 });
 
+//close mennu
 closeBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("is-open");
   openBtn.setAttribute("aria-expanded", false);
@@ -20,3 +22,14 @@ window.matchMedia("(min-width: 1200px)").addEventListener("change", (e) => {
   openBtn.setAttribute("aria-expanded", false);
   document.body.classList.remove("no-scroll");
 });
+
+//close menu
+headerNavList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("js-header-nav-link")) {
+    mobileMenu.classList.remove("is-open");
+    openBtn.setAttribute("aria-expanded", false);
+    document.body.classList.remove("no-scroll");
+  }
+});
+
+//.js-header-nav-link
