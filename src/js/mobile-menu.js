e18c1eb368f78e58a0d731f096a9e647bd9d909c
Anton <a.chertok@akatech.com.ua>
2023-06-28 12:08:53 +0300

    Update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -4,40 +4,39 @@ const closeBtn = document.querySelector(".js-close-menu");
 const headerNavList = document.querySelector("#js-header-nav-list");
 const ctaBtn = document.querySelector("#js-cta-button");
 
+const closeMenu = () => {
+  mobileMenu.classList.remove("is-open");
+  openBtn.setAttribute("aria-expanded", false);
+  document.body.classList.remove("no-scroll");
+};
+
 openBtn.addEventListener("click", () => {
   mobileMenu.classList.add("is-open");
   openBtn.setAttribute("aria-expanded", true);
   document.body.classList.add("no-scroll");
 });
 
-//close mennu
 closeBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("is-open");
-  openBtn.setAttribute("aria-expanded", false);
-  document.body.classList.remove("no-scroll");
+  closeMenu();
 });
 
 window.matchMedia("(min-width: 1200px)").addEventListener("change", (e) => {
   if (!e.matches) return;
-  mobileMenu.classList.remove("is-open");
-  openBtn.setAttribute("aria-expanded", false);
-  document.body.classList.remove("no-scroll");
+  closeMenu();
 });
 
-//close menu
 headerNavList.addEventListener("click", (e) => {
   if (e.target.classList.contains("js-header-nav-link")) {
-    mobileMenu.classList.remove("is-open");
-    openBtn.setAttribute("aria-expanded", false);
-    document.body.classList.remove("no-scroll");
+    closeMenu();
   }
 });
 
 ctaBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("is-open");
-  openBtn.setAttribute("aria-expanded", false);
-  document.body.classList.remove("no-scroll");
+  closeMenu();
 });
 
-//.js-header-nav-link
-//js-cta-button
+window.addEventListener("keydown", (e) => {
+  if (e.code === "Escape") {
+    console.log("esc");
+  }
+});
