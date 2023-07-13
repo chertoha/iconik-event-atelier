0a250f74e1bbd8080276d55d03e5cdd4a9003b25
Anton <a.chertok@akatech.com.ua>
2023-07-13 16:26:58 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,7 @@ import "./services-nav-select";
 import "./services-swiper";
 import "./features-swiper";
 import "./membership";
-// import "./gallery";
 import "./contacts-swiper";
 import "./events-swiper";
-// import "./map-frame";
 import "./mapbox";
 import "./progress-pagintator";
-
-// console.log(process.env.MAPBOX_ACCESS_TOKEN);
